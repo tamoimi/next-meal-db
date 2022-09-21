d1c48c68dff4f8b6eec5110194d871cae4deaf36
tami <xorud7979@gmail.com>
2022-09-21 16:56:31 +0900

    feat: API_KEY 로 데이터 받아오기

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,32 @@
+import { useEffect, useState } from "react";
 
-const mealForm = () => {
+export default function PeopleList({  }) {
+  const [people, setPeople] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const data = await (
+        await fetch(
+          `https://api.themoviedb.org/3/person/popular?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US&page=1`
+        )
+      ).json();
+      console.log(data);
+    })();
+  }, []);
+  // 데이터를 받아오고 있음 
 
+//   const getPeople = async () => {
+//     const result = await (
+//       await fetch(
+//         `https://api.themoviedb.org/3/person/popular?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US&page=1`
+//       )
+//     ).json();
+//     console.log("people response => ", result);
 
-return (
-<>
-안녕안녕
-</>
-);
+//     return result.results;
+//   };
 
+  return;
+  <>
+    <div className="container">{}</div>
+  </>;
 }
-
-export default mealForm 
