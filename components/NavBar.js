4339eafd8844f3c21c0b5680362804eefab8e156
tami <xorud7979@gmail.com>
2022-09-21 10:08:39 +0900

    feat: Link 를 이용해 NavBar의 메뉴 경로 설정
    - nav의 메뉴를 클릭하면  active 된 메뉴는 색이 바뀌고 클릭한 경로로 이동 가능

---
+++ components/NavBar.js
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function NavBar() {
+  const router = useRouter();
+  return (
+    <nav>
+      <Image src="/vercel.svg" alt="logo" width={100} height={50} />
+      <div>
+        <Link href="/">
+          <a className={router.pathname === "/" ? "active" : ""}>Home</a>
+        </Link>
+        <Link href="/about">
+          <a className={router.pathname === "/about" ? "active" : ""}>About</a>
+        </Link>
+      </div>
+      <style jsx>{`
+        nav {
+          display: flex;
+          gap: 10px;
+          flex-direction: column;
+          align-items: center;
+          padding-top: 20px;
+          padding-bottom: 10px;
+        }
+        nav a {
+          font-weight: 600;
+          font-size: 18px;
+        }
+        .active {
+          color: tomato;
+        }
+        nav div {
+          display: flex;
+          gap: 10px;
+        }
+      `}</style>
+    </nav>
+  );
+}
